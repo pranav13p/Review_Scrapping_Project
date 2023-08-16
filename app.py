b3b0eb45c3327bb25c865d4fb57e914da16a1d25
pranav13p <pranavrajput.2001@gmail.com>
2023-08-16 13:29:40 +0530

    First Commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import pymongo
+
 
 app = Flask(__name__)
 
@@ -19,6 +21,7 @@ def index():
             searchString = request.form['content'].replace(" ","")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
+
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
@@ -72,6 +75,12 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+
+            client = pymongo.MongoClient(" mongodb+srv://pranav123:mongodb@13p@cluster0.9ikuthc.mongodb.net/?retryWrites=true&w=majority")
+            db = client['review_scrap']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
@@ -83,4 +92,4 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+     app.run(host="0.0.0.0")
